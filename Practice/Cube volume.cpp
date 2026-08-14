566adf8bce9d34ac0827d4fc94b6a680202ef4f4
Swati0802 <25it445@bvmengineering.ac.in>
2026-08-14 22:18:15 +0530

    Create Cube volume.cpp

---
+++ Practice/Cube volume.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int cubevolume(int l=5, int w=6, int h=7)
+{
+    return l*w*h;
+}
+int main(){
+    cout<<cubevolume()<<endl;
+    cout<<cubevolume(9)<<endl;
+    cout<<cubevolume(15,12)<<endl;
+    cout<<cubevolume(3,4,7)<<endl;
+    return 0;
+}
